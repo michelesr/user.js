bfe19ad149080865851f90aa709829ec6d95e1e2
Michele Sorcinelli <michelesr@autistici.org>
2025-04-25 00:21:06 +0100

    Add DNS override

--- user-overrides.js
+++ user-overrides.js
@@ -1,3 +1,4 @@
 // USER OVERRIDES
 // user_pref("privacy.sanitize.sanitizeOnShutdown", false);
+user_pref("network.trr.mode", 3);
 user_pref("_user.js.parrot", "SUCCESS: Applied user.js overrides");
